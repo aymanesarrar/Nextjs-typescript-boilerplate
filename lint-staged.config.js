3e907ec949427c7018a4b7afb5c9f7aaef70c0cc
aymane <aymensarrar1@gmail.com>
2022-01-17 19:47:04 +0100

    init template

---
+++ lint-staged.config.js
@@ -0,0 +1,15 @@
+// lint-staged.config.js
+module.exports = {
+	// Type check TypeScript files
+	"**/*.(ts|tsx)": () => "npx tsc --noEmit",
+
+	// Lint then format TypeScript and JavaScript files
+	"**/*.(ts|tsx|js)": (filenames) => [
+		`npx eslint --fix ${filenames.join(" ")}`,
+		`npx prettier --write ${filenames.join(" ")}`,
+	],
+
+	// Format MarkDown and JSON
+	"**/*.(md|json)": (filenames) =>
+		`npx prettier --write ${filenames.join(" ")}`,
+};
